a82a1374c7cbb17dfc80df8bee7fe10f089e638b
ffukamizu <f.fukamizu@live.com>
2023-05-07 11:49:10 -0300

    reac components created

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+import App from './components/App'
+
+const app = App();
+const elementRoot = document.querySelector(".root");
+
+ReactDOM.render(app, elementRoot);
